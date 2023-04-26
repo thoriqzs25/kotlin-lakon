04cca8b47fddab40c6b9b802df0f48904ba92087
Thariq Zhafran Satyagraha <71017985+thoriqzs25@users.noreply.github.com>
2023-04-27 01:23:48 +0700

    chore: add auth slice

--- app/src/main/java/com/thariqzs/lakon/activity/AuthActivity.kt
+++ app/src/main/java/com/thariqzs/lakon/activity/AuthActivity.kt
@@ -2,11 +2,45 @@ package com.thariqzs.lakon.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.thariqzs.lakon.R
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import com.thariqzs.lakon.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    private var _binding: ActivityAuthBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        supportActionBar?.hide()
+        _binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setListeners()
+    }
+
+    private fun setListeners() {
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // This method is called before the text is changed.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // This method is called when the text is changed.
+                Log.d(TAG, "onTextChanged: $s")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // This method is called after the text is changed.
+            }
+        }
+//        binding.etEmail.addTextChangedListener(textWatcher)
+//        binding.etPassword.addTextChangedListener(textWatcher)
+//        binding.btnSignIn.setOnClickListener {
+//            Log.d(TAG, "onCreate: ${binding.etEmail.text} ${binding.etPassword.text}")
+//        }
+    }
+    companion object {
+        val TAG = "authactivitythoriq"
     }
 }
---
+++ app/src/main/java/com/thariqzs/lakon/ui/fragment/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.thariqzs.lakon.ui.fragment.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
